9a029cb3962a6d3e5c1cdc4e2314b01fdc0b89d0
DORSCORD <a.turchin08@gmail.com>
2023-12-16 15:11:11 +0100

    Fix bags

--- cards.py
+++ cards.py
@@ -1,48 +1,64 @@
 import easygui as gui
 
+
 class Card:
-    """Одна гральна карта"""
-    RANKS = [
-        "T", "2", "3", "4", "5", 
-        "6", "7", "8","9", 
-        "10", "В", "Д", "К"
-    ]
-    SUITS = [u'\u2660', u'\u2663', u'\u2665', u'\u2666']
-    
-    def __init__(self, rank, suit) -> None:
+    """Одна гральна карта."""
+
+    RANKS = ["Т", "2", "3", "4", "5", "6", "7", "8", "9", "10", "В", "Д", "K"]
+    SUITS = ["\u2660", "\u2663", "\u2665", "\u2666"]  # ♠ ♣ ♥ ♦
+    SUIT_LETTERS = "schd"
+
+    def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
-        
-    def __str__(self) -> str:
-        return self.rank + self.suit
-    
-class UnPrintableCard(Card):
-    """Карта, номінал якої не можна надрукувати"""
-    def __str__(self) -> str:
-        return "<Не можна надрукувати>"
-    
+
+    def __str__(self):
+        rep = self.rank + self.suit
+        return rep
+
+
+class Unprintable_Card(Card):
+    """Карта, номінал та масть якої не можуть бути виведені на екран."""
+
+    def __str__(self):
+        return "<не можна надрукувати>"
+
+
 class Positionable_Card(Card):
-    """Карта, яку можна перегортати"""
-    def __init__(self, rank, suit, face_up = True) -> None:
+    """Карта, яку можна покласти обличчям чи сорочкою нагору."""
+
+    def __init__(self, rank, suit, face_up=True):
         super().__init__(rank, suit)
         self.is_face_up = face_up
-    
-    def __str__(self) -> str:
+
+    def __str__(self):
         if self.is_face_up:
-            return super().__str__()
+            rep = super().__str__()
         else:
-            return "XX"
-        
+            rep = "XX"
+        return rep
+
     def flip(self):
         self.is_face_up = not self.is_face_up
-        
-    
+
+    @property
+    def image_filename(self):
+        if not self.is_face_up:
+            filename = "back.png"
+        else:
+            suit_name = self.SUIT_LETTERS[self.SUITS.index(self.suit)]
+            rank_name = self.RANKS.index(self.rank) + 1
+            filename = f"{suit_name}{rank_name:0>2}.png"
+        return "assets/" + filename
+
+
 class Hand:
-    """Рука: Набір карт на руках для кожного гравця """
-    def __init__(self) -> None:
+    """Рука: набір карт на руках одного гравця."""
+
+    def __init__(self):
         self.cards = []
-        
-    def __str__(self) -> str:
+
+    def __str__(self):
         if self.cards:
             rep = ""
             for card in self.cards:
@@ -50,66 +66,75 @@ class Hand:
         else:
             rep = "<пусто>"
         return rep
-    
+
+    @property
+    def card_images(self):
+        return [card.image_filename for card in self.cards]
+
     def clear(self):
-        # self.cards.clear()
         self.cards = []
-        
+
     def add(self, card):
         self.cards.append(card)
-        
+
     def give(self, card, other_hand):
         self.cards.remove(card)
         other_hand.add(card)
-        
+
 
 class Deck(Hand):
-    """Колода гральнх карт"""
-    
+    """Колода гральних карт."""
+
     def populate(self):
         for suit in Card.SUITS:
             for rank in Card.RANKS:
                 self.add(Card(rank, suit))
-                
+
     def shuffle(self):
-        from random import shuffle as shf
-        shf(self.cards)
-        
+        import random
+
+        random.shuffle(self.cards)
+
     def add_new_deck(self):
         self.populate()
         self.shuffle()
-        
-    def deal(self, hands, per_hand = 1):
+
+    def deal(self, hands, per_hand=1):
         for rounds in range(per_hand):
             for hand in hands:
+                # Перевірка на наявність карт у колоді
                 if not self.cards:
                     self.add_new_deck()
                 top_card = self.cards[0]
-                self.give(top_card,  hand)
-                
+                self.give(top_card, hand)
+
+
 if __name__ == "__main__":
-    gui.msgbox("Ви запустили модуль cards, а не імпортували його.")
-    gui.msgbox("Тестування модуля")
-    card1 = Card("T", Card.SUITS[0])
-    card2 = UnPrintableCard("T", Card.SUITS[1])
-    card3 = Positionable_Card("T", Card.SUITS[2])
-    gui.msgbox(f"Об'єкт Card: " + str(card1))
-    gui.msgbox(f"Об'єкт UnPrintableCard:"  + str(card2))
-    gui.msgbox(f"Об'єкт Positionable_Card:" + str(card3))
+    gui.msgbox("Ви запустили модуль cards, " "а не імпортували його (import cards).")
+    gui.msgbox("Тестування модуля.\n")
+
+    card1 = Card("Т", Card.SUITS[0])
+    card2 = Unprintable_Card("Т", Card.SUITS[1])
+    card3 = Positionable_Card("Т", Card.SUITS[2])
+    gui.msgbox("Об'єкт Card: " + str(card1))
+    gui.msgbox("Об'єкт Unprintable_Card: " + str(card2))
+    gui.msgbox("Об'єкт Positionable_Card: " + str(card3))
     card3.flip()
-    gui.msgbox(f"Перевернутий об'єкт Positionable_Card" + str(card3))
+    gui.msgbox("Перевертаю об'єкт Positionable_Card: " + str(card3))
+
     deck1 = Deck()
-    gui.msgbox(f"Створено нову колоду:"  + str(deck1))
+    gui.msgbox("\nСтворено нову колоду: " + str(deck1))
     deck1.populate()
-    gui.msgbox(f"У колоді з'явилися карти:"  + str(deck1))
+    gui.msgbox("У колоді з'явилися карти: " + str(deck1))
     deck1.shuffle()
-    gui.msgbox(f"Перемішана колода:" +str(deck1))
+    gui.msgbox("Колода перемішана: " + str(deck1))
+
     hand1 = Hand()
     hand2 = Hand()
     deck1.deal(hands=(hand1, hand2), per_hand=5)
-    gui.msgbox("Роздано по 5 карт")
-    gui.msgbox(f"Рука1 " + {hand1})
-    gui.msgbox(f"Рука2:" + {hand2})
-    gui.msgbox(f"Залишилось в колоді: {deck1}", sep = "\n")
+    gui.msgbox("Роздано по 5 карт.")
+    gui.msgbox("Рука1: " + str(hand1))
+    gui.msgbox("Рука2: " + str(hand2))
+    gui.msgbox("Залишилось у колоді: " + str(deck1))
     deck1.clear()
-    gui.msgbox(f"Колода очищена {deck1}")
+    gui.msgbox("Колода очищена: " + str(deck1))
--- games.py
+++ games.py
@@ -1,16 +1,22 @@
 import easygui as gui
 
-def ask_yes_no(question, title = "Game"):
-    return gui.ynbox(question + "? ", title)
 
-def ask_number(question, low, high, title= "Game"):
-    response = 0
-    while response not in range(low, high+1):
-        response = int(input(question))
-    return gui.integerbox(question, title, default=int((low+high)/2), lowerbound=low, upperbound = high)
+def ask_yes_no(question, title="Game"):
+    """Задає питання з відповіддю (y/n)."""
+    return gui.ynbox(question + "?", title)
+
+
+def ask_number(question, low, high, title="Game"):
+    """Просить ввести число із заданого діапазону."""
+    return gui.integerbox(
+        question, title, default=int((low + high) / 2), lowerbound=low, upperbound=high
+    )
+
 
 if __name__ == "__main__":
-    gui.msgbox("Ви запустили модуль games, а не імпортували його (import games)")
-    gui.msgbox("Тестування модуля ...")
-    gui.msgbox(f"Функція ask_yes_no повернула: {ask_yes_no('Продовжуємо тестування ...')}")
-    gui.msgbox(f"Функція ask_number повернула: {ask_number('Введіть число від 1 до 10: ', 1, 10)}")
+    gui.msgbox("Ви запустили модуль games, " "а не імпортували його (import games).")
+    gui.msgbox("Тестування модуля.")
+    answer = ask_yes_no("Продовжуємо тестування")
+    gui.msgbox("Функція ask_yes_no повернула " + str(answer))
+    answer = ask_number("Введіть ціле число від 1 до 10:", 1, 10)
+    gui.msgbox("Функція ask_number повернула " + str(answer))
