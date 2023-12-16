1730ed3725ff1928d46dbd9fd8ded3f9e4495ab6
DORSCORD <a.turchin08@gmail.com>
2023-12-16 13:51:26 +0100

    Add GUI in Black-Jack

--- blackjack.py
+++ blackjack.py
@@ -1,10 +1,17 @@
-import cards, games
+import easygui as gui
 
-MIN_BET = 1
+import cards
+import games
+
+
+TITLE = "Блек Джек"
+
 
 class BJ_Card(cards.Positionable_Card):
+    """Карта для гри в Блек-джек."""
+
     ACE_VALUE = 1
-    
+
     @property
     def value(self):
         if self.is_face_up:
@@ -14,109 +21,108 @@ class BJ_Card(cards.Positionable_Card):
         else:
             v = None
         return v
-    
+
+
 class BJ_Deck(cards.Deck):
+    """Колода для гри в Блек-джек."""
+
     def populate(self):
         for suit in BJ_Card.SUITS:
             for rank in BJ_Card.RANKS:
                 self.cards.append(BJ_Card(rank, suit))
-                
+
 
 class BJ_Hand(cards.Hand):
-    def __init__(self, name) -> None:
+    """Рука гравця в Блек-джек."""
+
+    def __init__(self, name):
         super().__init__()
         self.name = name
-        
-    def __str__(self) -> str:
-        rep = self.name  + ":\t" + super().__str__()
+
+    def __str__(self):
+        rep = self.name + ":\t" + super().__str__()
         if self.total:
             rep += "(" + str(self.total) + ")"
         return rep
-    
+
     @property
     def total(self):
+        # якщо value однієї з карт дорівнює None,
+        # то і властивість в цілому дорівнює None
         for card in self.cards:
             if not card.value:
                 return None
+
+        # сумуємо очки, рахуючи кожен туз за 1 очко
+        # визначаємо, чи є туз на руках у гравця
         t = 0
         contains_ace = False
         for card in self.cards:
             t += card.value
             if card.value == BJ_Card.ACE_VALUE:
                 contains_ace = True
-        
-        if contains_ace and  t <= 11:
+
+        # якщо на руках є туз і сума очок не перевищує 11,
+        # будемо рахувати туз за 11 очок
+        if contains_ace and t <= 11:
+            # додати потрібно лише 10,
+            # тому що одиниця вже увійшла до загальної суми
             t += 10
-        
+
         return t
-    
-    def is_busted(self) -> bool:
-        return self.total > 21 
-    
+
+    def is_busted(self):
+        return self.total > 21
+
+    def show_message(self, message):
+        gui.msgbox(str(self) + "\n" + message, TITLE)
+
 
 class BJ_Player(BJ_Hand):
-    """Гравець у Блек-Джек"""
+    """Гравець у Блек-джек."""
 
-    def __init__(self, name, money):
-        super().__init__(name)
-        self.money = money
-    
     def is_hitting(self):
-        response = games.ask_yes_no("\n" + self.name + ", братиме ще карти")
-        return response == "y"
-    
+        response = games.ask_yes_no(
+            str(self) + "\n" + self.name + ", братимете ще карти"
+        )
+        return response
+
     def bust(self):
-        print(f"{self.name} перебрав(-ла).")
-        self.lose()
-        
+        self.show_message(self.name + " перебрав(ла).")
+
     def lose(self):
-        print(f"{self.name} програв(-ла).")
-        
-    def win(self):
-        self.money += 2 * self.bet_value
-        print(f"{self.name} виграв(-ла).")
-        
-    def push(self):
-        self.money += self.bet_value
-        print(f"{self.name} зіграв(-ла) з дилером в нічию.")
-    
-    def bet(self, bet_value):
-        if bet_value > self.money:
-            return False
-        self.bet_value = bet_value
-        self.money -= self.bet_value
-        return bet_value
-    
-    def escape(self):
-        self.money += int(self.bet_value * 0.5)
-        self.clear()
-        print(f"{self.name} вийшов(ла) з гри зі збереженням половини ставки")
+        self.show_message(self.name + " програв(ла).")
 
-    def is_escaped(self):
-        return len(self.cards) == 0
-    
-        
+    def win(self):
+        self.show_message(self.name + " виграв(ла).")
+
+    def push(self):
+        self.show_message(self.name + " зіграв(ла) з дилером внічию.")
+
+
 class BJ_Dealer(BJ_Hand):
-    
+    """Дилер у Блек-джек."""
+
     def is_hitting(self):
-        return self.total < 17 
-    
+        return self.total < 17
+
     def bust(self):
-        print(f"{self.name} перебрав.")
-        
+        self.show_message(self.name + " перебрав.")
+
     def flip_first_card(self):
         first_card = self.cards[0]
         first_card.flip()
-        
-        
+
+
 class BJ_Game:
-    
-    def __init__(self, players):
-        self.players: list[BJ_Player] = []
-        for name, money in players.items():
-            player = BJ_Player(name, money)
+    """Гра Блек-джек."""
+
+    def __init__(self, names):
+        self.players = []
+        for name in names:
+            player = BJ_Player(name)
             self.players.append(player)
-            
+
         self.dealer = BJ_Dealer("Дилер")
 
         self.deck = BJ_Deck()
@@ -124,63 +130,48 @@ class BJ_Game:
 
     @property
     def still_playing(self):
-        sp: list[BJ_Player] = []
+        sp = []
         for player in self.players:
             if not player.is_busted():
                 sp.append(player)
         return sp
-    
-    def __additional_cards(self, player: BJ_Player):
+
+    def __additional_cards(self, player):
         while not player.is_busted() and player.is_hitting():
             self.deck.deal([player])
-            print(player)
             if player.is_busted():
                 player.bust()
 
-    def __betting(self):
-        for player in self.players.copy():
-            if player.money < MIN_BET:
-                self.players.remove(player)
-                print(f"Гравець {player.name} видалення з капіталом {player.money}")
-                continue
-            bet_value = games.ask_number(
-            f"Ваша ставка {player.name} ({MIN_BET} - {player.money}): ", 
-            MIN_BET,
-            player.money
-            )
-            player.bet(bet_value)
-    
-    def __escaping(self, player):
-        answer = games.ask_yes_no(f"{player}, чи будете Ви грати?")
-        if answer == "n":
-            player.escaped()
-            return True
-        return False
-                
     def play(self):
-        self.__betting()
-        if len(self.deck.cards) < (len(self.players) + 1) * 2:
-            self.add_new_deck()
-
+        # здача всім по дві карти
         self.deck.deal(self.players + [self.dealer], per_hand=2)
         self.dealer.flip_first_card()
+        # перша з карт, зданих дилеру, перевертається
+        message = "\n".join(str(player) for player in self.players + [self.dealer])
+        gui.msgbox(message, TITLE)
+
+        # роздавання додаткових карт гравцям
         for player in self.players:
-            print(player)
-        print(self.dealer)
-        for player in self.players:
-            if not self.__escaping(player):
-                self.__additional_cards(player)
+            self.__additional_cards(player)
+        # перша карта дилера розкривається
         self.dealer.flip_first_card()
+
         if not self.still_playing:
-            print(self.dealer)
-        
+            # всі гравці перебрали,
+            # покажемо лише "руку" дилера
+            self.dealer.show_message(self.dealer.name + " переміг!")
         else:
-            print(self.dealer)
+            # роздача додаткових карт дилеру
             self.__additional_cards(self.dealer)
+
             if self.dealer.is_busted():
+                # виграють усі, хто ще залишився у грі
                 for player in self.still_playing:
                     player.win()
             else:
+                self.dealer.show_message(self.dealer.name + " набрав додаткові карти.")
+                # порівнюємо суми очок у дилера
+                # та у гравців, що залишилися у грі
                 for player in self.still_playing:
                     if player.total > self.dealer.total:
                         player.win()
@@ -188,29 +179,32 @@ class BJ_Game:
                         player.lose()
                     else:
                         player.push()
-        
+
+        # видалення всіх карт
         for player in self.players:
             player.clear()
         self.dealer.clear()
-        
+
+
 def main():
-    print("\t\tЛаскава просимо до гри Блек-Джек\n")
+    gui.msgbox("Ласкаво просимо до гри Блек-джек!", TITLE)
 
-    players = {}
-    number = games.ask_number("Скільки всього гравців (1-7): ", low=1, high=7)
-    for i in range(number):
-        name = input(f"Введіть ім'я гравця №{i+1}: ")
-        money = games.ask_number(f"Введіть капітал гравця {name}: ", MIN_BET, MIN_BET * 100
-        )
-        players[name] = money
-    
-    print()
-    
-    game = BJ_Game(players)
+    names = []
+    number = games.ask_number("Скільки всього гравців? (1 - 7): ", low=1, high=7)
+    if number is None:
+        exit()
+    fields = ["Гравець " + str(i + 1) for i in range(number)]
+    names = gui.multenterbox(
+        "Введіть імена гравців", TITLE, fields=fields, values=fields
+    )
+    if names is None:
+        exit()
+    game = BJ_Game(names)
 
-    again =None
-    while again != "n":
+    again = True
+    while again:
         game.play()
-        again = games.ask_yes_no("\nБажання зіграти ще раз ")
-    
+        again = games.ask_yes_no("Бажаєте зіграти ще раз", TITLE)
+
+
 main()
